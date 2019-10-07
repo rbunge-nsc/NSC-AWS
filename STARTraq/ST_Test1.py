fd34d22bf4aed18f2ba14d77cb7da21d7359afa5
rbunge-nsc <robert.bunge@seattlecolleges.edu>
2019-10-07 21:31:54 +0000

    Testing user input, including a user selected file name

--- STARTraq/ST_Test1.py
+++ STARTraq/ST_Test1.py
@@ -1,10 +1,18 @@
 #print("Hello World!")
 print("Good-bye World!")
 
-f = open("/home/ec2-user/environment/NSC-AWS/STARTraq/frog.txt", "x")
-f.write("Ribbit!")
+fname = input("Enter your file name: ") 
+msg = input("Enter your message: ")
+
+f = open("/home/ec2-user/environment/NSC-AWS/STARTraq/ {} .txt".format(fname), "x")
+f.write(msg)
 f.close()
 
-f = open("/home/ec2-user/environment/NSC-AWS/STARTraq/frog.txt", "r")
+f = open("/home/ec2-user/environment/NSC-AWS/STARTraq/ {} .txt".format(fname), "r")
 print(f.read())
 f.close()
+
+
+#>>> shepherd = "Mary"
+#>>> age = 32
+#>>> stuff_in_string = "Shepherd {} is {} years old.".format(shepherd, age)
